3d33b23dd5f6ee59395090a3273337c052bcae44
jabborov02 <your-email@example.com>
2024-09-08 23:55:48 -0400

    Search

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from django.db.models import Q
+from .models import Room, Topic
 from .forms import RoomForm
 
 # Create your views here.
@@ -12,8 +13,19 @@ from .forms import RoomForm
 
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {"rooms": rooms}
+    q = request.GET.get("q") if request.GET.get('q') != None else ''
+    
+    
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q)|
+        Q(description__icontains=q)
+        )
+
+    topics = Topic.objects.all()
+    room_count = rooms.count()
+
+    context = {"rooms": rooms, "topics": topics, "room_count": room_count}
     return render(request, "base/home.html", context)
 
 
@@ -56,6 +68,6 @@ def deleteRoom(request, pk):
     room = Room.objects.get(id=pk)
     if request.method == "POST":
         room.delete()
-        return redirect('home')
-    
+        return redirect("home")
+
     return render(request, "base/delete.html", {"obj": room})
