54e82e813592abb7a0ed1ac6fc6ed407a6983f97
jabborov02 <your-email@example.com>
2024-09-22 17:39:18 -0400

    5:35:02 - Mobile Responsiveness

--- base/urls.py
+++ base/urls.py
@@ -7,8 +7,6 @@ urlpatterns = [
     path('logout/', views.logoutUser, name="logout"),
     path('register/', views.registerPage, name="register"),
 
-
-    
     path("", views.home, name="home"),
     path("room_page/<str:pk>", views.room, name="room"),
     path('profile/<str:pk>/', views.userProfile, name="user-profile"),
@@ -17,6 +15,8 @@ urlpatterns = [
     path('update-room/<str:pk>/', views.updateRoom, name="update-room"),
     path('delete-room/<str:pk>/', views.deleteRoom, name="delete-room"),
     path('delete-message/<str:pk>/', views.deleteMessage, name="delete-message"),
+
+    path('update-user/', views.updateUser, name="update-user"),
 
 
 
